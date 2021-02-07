a5722be9a7b508bf526be4fb6b67c84a0ddf7373
on-mmr <o.h.mamoru@gmail.com>
2021-02-07 18:07:38 +0900

    Add navigation

--- my-app/src/app/hero-detail/hero-detail.component.ts
+++ my-app/src/app/hero-detail/hero-detail.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import {Hero} from '../hero';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-hero-detail',
@@ -10,9 +13,29 @@ export class HeroDetailComponent implements OnInit {
 
   @Input() hero: Hero | undefined;
 
-  constructor() { }
+  constructor(
+    private route : ActivatedRoute,
+    private heroService : HeroService,
+    private location : Location
+  ) { }
 
   ngOnInit(): void {
+    this.getHero();
+  }
+  
+  getHero(): void {
+    let getId = this.route.snapshot.paramMap.get('id')
+    let id: number;
+    if(getId != null){
+      id = +getId;
+      this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero);
+    }else{
+      throw new Error('idが存在しません');
+    }
   }
 
+  goBack(): void {
+    this.location.back();
+  }
 }
